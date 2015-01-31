0fd672af7f8d3e8aca14d95f5dee43c419dc3f94
Bert Timmermans <Bert@Berts-Macbook-Pro.local>
2015-01-31 20:43:43 +0100

    Move

--- Dev/js/controls.js
+++ Dev/js/controls.js
@@ -21,19 +21,39 @@
     this.camera = this.instance.camera;
     this.events = events();
     this.input = (this.instance.touch)? new TouchControls(this) : new KeyboardControls(this);
-    this.input = new TouchControls(this);
-    this.input.init();
+    this.input.init();    
+    
+    console.log( '---- 0' );
+
+    console.log( 'Cos ' + Math.cos((90 * Math.PI) / 180) );
+    console.log( 'Sin ' + Math.sin((90 * Math.PI) / 180) );
+    
+
+    console.log( '---- 90' );
+
+    console.log( 'Cos ' + Math.cos((0 * Math.PI) / 180) );
+    console.log( 'Sin ' + Math.sin((0 * Math.PI) / 180) );
+    
+    
+    console.log( '---- 180' );
+    
+    console.log( 'Cos ' + Math.cos((-90 * Math.PI) / 180) );
+    console.log( 'Sin ' + Math.sin((-90 * Math.PI) / 180) );
+
+    
+    console.log( '---- 270' );
+
+    console.log( 'Cos ' + Math.cos((-180 * Math.PI) / 180) );
+    console.log( 'Sin ' + Math.sin((-180 * Math.PI) / 180) );
+    
+    console.log( '---- 359' );
+
+    console.log( 'Cos ' + Math.cos((-270 * Math.PI) / 180) );
+    console.log( 'Sin ' + Math.sin((-270 * Math.PI) / 180) );
     
   }
   
   Controls.prototype.validate = (function(){
-    
-    // Movement
-    
-    if(this.events.right) this.camera.x += 1*this.speed;
-    if(this.events.left) this.camera.x -= 1*this.speed;
-    if(this.events.up) this.camera.z += 1*this.speed;
-    if(this.events.down) this.camera.z -= 1*this.speed;
     
     // Camera rotation
     
@@ -47,6 +67,27 @@
     
     this.events.pan.x = 0;
     this.events.pan.y = 0;
+    
+    var angle = ((((this.camera.ry+Math.PI)/2)/Math.PI)*-360)+180;
+    
+    // Movement
+    
+    if(this.events.down) {
+      this.camera.x += -1*(this.speed * Math.sin((-angle) * Math.PI / 180));
+      this.camera.z += -1*(this.speed * Math.cos((-angle) * Math.PI / 180));
+    }
+    if(this.events.up) {
+      this.camera.x += 1*(this.speed * Math.sin((-angle) * Math.PI / 180));
+      this.camera.z += 1*(this.speed * Math.cos((-angle) * Math.PI / 180));
+    }
+    if(this.events.right) {
+      this.camera.x += 1*(this.speed * Math.cos((angle) * Math.PI / 180));
+      this.camera.z += 1*(this.speed * Math.sin((angle) * Math.PI / 180));
+    }
+    if(this.events.left) {
+      this.camera.x += -1*(this.speed * Math.cos((angle) * Math.PI / 180));
+      this.camera.z += -1*(this.speed * Math.sin((angle) * Math.PI / 180));
+    }
     
   });
 
