0ccfb9414a694d373bf2d89981e940473dc47016
Bert Timmermans <Bert@Berts-Macbook-Pro.local>
2015-02-08 14:12:32 +0100

    Place player in center of world on load

--- Dev/js/render.js
+++ Dev/js/render.js
@@ -32,6 +32,9 @@
     
     // Setup Three JS camera
 		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.05, 10000 );
+		this.cameraData.x = parseInt((data[0].length/2)*this.size);
+		this.cameraData.z = parseInt((data.length/2)*this.size);
+		console.log(this.cameraData);
 		this.camera.rotation.order = "YXZ";
     this.scene.add(this.camera);
     
